a9d7e61a54a62d40eea33cb8f02c4ee72fb9a89b
Soumya Ray <soumya.ray@gmail.com>
2019-05-17 20:40:33 +0800

    Verify email using reg token; return auth_token to API on requests

---
+++ app/services/get_all_projects.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'http'
+
+# Returns all projects belonging to an account
+class GetAllProjects
+  def initialize(config)
+    @config = config
+  end
+
+  def call(current_account)
+    response = HTTP.auth("Bearer #{current_account.auth_token}")
+                   .get("#{@config.API_URL}/projects")
+
+    response.code == 200 ? response.parse['data'] : nil
+  end
+end
